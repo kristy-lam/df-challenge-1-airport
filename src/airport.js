e63f648686faf24c74586b0cc18fa66317c3e2f4
Kristy Lam <lamlokhinkristy@gmail.com>
2024-04-19 15:57:11 +0100

    Updated production code to pass User Story 2 - Test 3.

--- src/airport.js
+++ src/airport.js
@@ -1,7 +1,7 @@
 export const airport = {    
     capacity: 10,  // default capacity used for testing
     modifyCapacity: function (newCapacity){
-        if (Number.isInteger(newCapacity)) {
+        if (Number.isInteger(newCapacity) && newCapacity >= 0) {
             this.capacity = newCapacity;
         }
     },    
