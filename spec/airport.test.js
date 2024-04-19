2d4586bcb0068d8c3f59cb57f2d99547e1357c27
Kristy Lam <lamlokhinkristy@gmail.com>
2024-04-19 14:51:07 +0100

    Starts User Story 2 - Test 1.

--- spec/airport.test.js
+++ spec/airport.test.js
@@ -9,9 +9,10 @@ const afterEach = () => {
 };
 
 // * User Story 1
+console.log(`USER STORY 1`);
+
 // ? Test 1: Check whether there is a representation of the airport's
 // ? maximum capacity of the number of planes that can be at the airport
-
 console.log(`Test 1`);
 console.log(`==================`);
 console.log(
@@ -36,4 +37,36 @@ console.log(`==================`);
 // Clean Up
 afterEach();
 
-//! END OF TEST 1
+//! END OF USER STORY 1 - TEST 1
+
+// * User Story 2
+console.log(`USER STORY 2`);
+
+// ? Test 1: Test that capacity can be modified accordingly
+console.log(`Test 1`);
+console.log(`==================`);
+console.log(
+    `Test that capacity can be modified accordingly`
+);
+
+// Arrange
+expected = 20;
+let testCapacity = 20;
+
+// Act
+modifyCapacity();
+actual = Object.values(airport.capacity);
+
+// Assert
+result = assertEquals(expected, actual);
+
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log(`==================`);
+
+// Clean Up
+afterEach();
+
+//! END OF USER STORY 2 - TEST 1
+
